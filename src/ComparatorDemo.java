61f903631348c0383cdf46eb84a86ad0cfa04df6
trishul.test.con <trishul.test.con@phonepe.com>
2022-03-30 20:28:56 +0530

    worked on comparable and comparator

--- src/ComparatorDemo.java
+++ src/ComparatorDemo.java
@@ -3,6 +3,20 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class ComparatorDemo {
+    public static void main(String[] args) {
+        ArrayList<Movies> list = new ArrayList<>();
+        list.add(new Movies("Force Awakens", 8.3, 2015));
+        list.add(new Movies("Star Wars", 8.7, 1977));
+        list.add(new Movies("Empire Strikes Back", 8.8, 1980));
+        list.add(new Movies("Return of the Jedi", 8.4, 1983));
+
+        Collections.sort(list,new NameComparator());
+
+        System.out.println(list);
+
+        Collections.sort(list,new RatingComparator());
+        System.out.println(list);
+    }
 }
 
 class Movies implements java.lang.Comparable<Movies> {
@@ -48,12 +62,7 @@ class RatingComparator implements Comparator<Movies>{
 
     @Override
     public int compare(Movies o1, Movies o2) {
-        if (o1.getRating() > o2.getRating())
-            return 1;
-        if (o1.getRating() < o2.getRating())
-            return -1;
-        else
-            return 0;
+        return Double.compare(o1.getRating(), o2.getRating());
     }
 }
 
@@ -62,22 +71,5 @@ class NameComparator implements Comparator<Movies>{
     @Override
     public int compare(Movies o1, Movies o2) {
         return o1.getMovie().compareTo(o2.getMovie());
-    }
-}
-
-class Main {
-    public static void main(String[] args) {
-        ArrayList<Movies> list = new ArrayList<>();
-        list.add(new Movies("Force Awakens", 8.3, 2015));
-        list.add(new Movies("Star Wars", 8.7, 1977));
-        list.add(new Movies("Empire Strikes Back", 8.8, 1980));
-        list.add(new Movies("Return of the Jedi", 8.4, 1983));
-
-        Collections.sort(list,new NameComparator());
-
-        System.out.println(list);
-
-        Collections.sort(list,new RatingComparator());
-        System.out.println(list);
     }
 }
